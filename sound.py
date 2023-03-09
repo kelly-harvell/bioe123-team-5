e1e8a3ba931adb7ac04320412beb5b194971210b
kelly-harvell <123216298+kelly-harvell@users.noreply.github.com>
2023-03-09 09:50:20 -0800

    Add files via upload
    - countdown now avaliable

--- sound.py
+++ sound.py
@@ -1,5 +1,6 @@
 import winsound
 import serial
+import time
 
 ser = serial.Serial('COM9', baudrate=9600, timeout=1)
 
@@ -10,6 +11,14 @@ def notification():
     winsound.Beep(880, 300)
     winsound.Beep(880, 800)
 
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
 centrifugetime = 0
 
 while(1):
@@ -18,6 +27,7 @@ while(1):
         centrifugetime = input("Input desired centrifuge spin cycle time in seconds: ")
         message = str('<' + setspeed + ',' + centrifugetime + '>')
         ser.write(str(message).encode('ascii'))
+        countdown(int(centrifugetime))
 
     #play notification sound when cycle is complete
     arduinoData = ser.readline().decode('ascii')
